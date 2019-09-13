3adf3aae69fa80f7221977aaa9065f6016fc42ba
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-13 03:43:06 +0000

    Automatically backed up by Learn

--- spec/generalized_map_and_reduce_spec.rb
+++ spec/generalized_map_and_reduce_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 describe 'my own map' do
   it "returns an array with all values made negative" do
-    expect(map1([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
+    expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
   end
 
   it "returns an array with the original values" do
     dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
-    expect(map2(dune){|n| n}).to eq(dune)
+    expect(map(dune){|n| n}).to eq(dune)
   end
 
   it "returns an array with the original values multiplied by 2" do
-    expect(map3([1, 2, 3, -9]){|n| n * 2}).to eq([2, 4, 6, -18])
+    expect(map([1, 2, 3, -9]){|n| n * 2}).to eq([2, 4, 6, -18])
   end
 
   it "returns an array with the original values squared" do
-    expect(map4([1, 2, 3, -9]){|n| n * n}).to eq([1, 4, 9, 81])
+    expect(map([1, 2, 3, -9]){|n| n * n}).to eq([1, 4, 9, 81])
   end
 end
 
