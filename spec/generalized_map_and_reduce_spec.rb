ddd70edcc7f6e56dc7f1c099b417fd1e4d7456c3
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-13 03:54:22 +0000

    Automatically backed up by Learn

--- spec/generalized_map_and_reduce_spec.rb
+++ spec/generalized_map_and_reduce_spec.rb
@@ -22,32 +22,32 @@ end
 describe 'my own reduce' do
   it "returns a running total when not given a starting point" do
     source_array = [1,2,3]
-    expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
+    expect(reduce1(source_array){|memo, n| memo + n}).to eq(6)
   end
 
   it "returns a running total when given a starting point" do
     source_array = [1,2,3]
     starting_point = 100
-    expect(reduce(source_array, starting_point){|memo, n| memo + n}).to eq(106)
+    expect(reduce2(source_array, starting_point){|memo, n| memo + n}).to eq(106)
   end
 
   it "returns true when all values are truthy" do
     source_array = [1, 2, true, "razmatazz"]
-    expect(reduce(source_array){|memo, n| memo && n}).to be_truthy
+    expect(reduce3(source_array){|memo, n| memo && n}).to be_truthy
   end
 
   it "returns false when any value is false" do
     source_array = [1, 2, true, "razmatazz", false]
-    expect(reduce(source_array){|memo, n| memo && n}).to be_falsy
+    expect(reduce4(source_array){|memo, n| memo && n}).to be_falsy
   end
 
   it "returns true when a truthy value is present" do
     source_array = [ false, nil, nil, nil, true]
-    expect(reduce(source_array){|memo, n| memo || n}).to eq(true)
+    expect(reduce5(source_array){|memo, n| memo || n}).to eq(true)
   end
 
   it "returns false when no truthy value is present" do
     source_array = [ false, nil, nil, nil]
-    expect(reduce(source_array){|memo, n| memo && n}).to eq(false)
+    expect(reduce6(source_array){|memo, n| memo && n}).to eq(false)
   end
 end
